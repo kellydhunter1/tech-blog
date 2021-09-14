249ec483b12d220826d5308079eec00aca379362
kellydhunter1 <kellydhunter1@gmail.com>
2021-09-14 13:42:52 -0400

    set up routes and started log in logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-  res.render('homepage');
+  console.log(req.session);
+  console.log('======================');
+  res.render('homepage', {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'Handlebars Docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
+
